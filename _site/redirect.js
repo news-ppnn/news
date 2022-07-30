339552f72ae0ebd6c205e41a13518a7c33b1997b
news-ppnn <blogitple@gmail.com>
2022-07-30 16:01:17 +0900

    admin-cofig-ver011

--- _site/redirect.js
+++
@@ -1,9 +0,0 @@
-// 리디렉션하기
-// netlify에 호스팅 했다면 도메인으로 연결한다. 
-let netlifyUrl = "hero-puzzle-game.netlify.app"
-let hostingUrl = "hero.ppnn.co.kr"
-if (location.href.includes(netlifyUrl)) {
-  let url = location.href;
-  url = url.replace(netlifyUrl, hostingUrl);
-  location.href = url;
-}
