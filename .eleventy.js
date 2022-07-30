6f5cd06e5258a23c6f57928269801320ccc6636a
news-ppnn <blogitple@gmail.com>
2022-07-30 12:39:49 +0900

    admin-cofig

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
+// njk 파일 등은 자동으로 복사된다.
 module.exports = function (config) {
   config.addPassthroughCopy("./input/public/");
+  config.addPassthroughCopy("./input/admin/");
   // 필요한 파일을 추가한다.
+  config.addPassthroughCopy("./input/data.js");
   config.addPassthroughCopy("./input/sw.js"); 
   config.addPassthroughCopy("./input/robots.txt"); 
-  config.addPassthroughCopy("./input/ads.txt");
+  config.addPassthroughCopy("./input/ads.txt"); 
   config.addPassthroughCopy("./input/redirect.js");
   config.addPassthroughCopy("./input/manifest.json");
   config.addWatchTarget("./input/public/");   
